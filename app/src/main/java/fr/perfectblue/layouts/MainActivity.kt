a0387bb114fb84c93b377c0044105a68e1b33e9f
Maxime <maxime.alfosea@hotmail.fr>
2019-04-30 16:20:14 +0200

    Intégration d'une cardView dans une GridView

--- app/src/main/java/fr/perfectblue/layouts/MainActivity.kt
+++ app/src/main/java/fr/perfectblue/layouts/MainActivity.kt
@@ -2,11 +2,37 @@ package fr.perfectblue.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_card.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
+
+    private var likeState: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cardLikeButton.setOnClickListener {
+            this.toggleLike()
+        }
+
+        cardWebButton.setOnClickListener {
+            browse("https://www.pokepedia.fr/Carapuce")
+        }
+    }
+
+    private fun toggleLike() {
+        if (this.likeState) {
+            cardLikeButton.setBackgroundResource(R.drawable.ic_like_idle)
+            this.likeState = false
+            toast("Retiré des favoris")
+        } else {
+            cardLikeButton.setBackgroundResource(R.drawable.ic_like_clicked)
+            this.likeState = true
+            toast("Ajouté aux favoris")
+        }
     }
 }
